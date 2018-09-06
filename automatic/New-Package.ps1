c6f944fb2548cb9e73ffd86c88026783b9f2051a
Nicholas Cecere <nick@thebitop.net>
2018-09-06 17:20:15 -0400

    Testing Auto Packaging

---
+++ automatic/New-Package.ps1
@@ -0,0 +1,64 @@
+param(
+    [Parameter(Mandatory = $true)]
+    $Name,
+    [ValidateSet('Portable', 'Installer')]
+    [string] $Type='Installer'
+)
+# Original Source: <https://github.com/majkinetor/chocolatey/blob/master/New-Package.ps1>
+
+<#
+.SYNOPSIS
+    Create a new package from the template
+.DESCRIPTION
+    This function creates a new package by using the directory _template which contains desired package basic settings.
+#>
+function New-Package{
+    [CmdletBinding()]
+    param(
+        #Package name
+        [string] $Name,
+
+        #Type of the package
+        [string] $Type,
+
+        #Github repository in the form username/repository
+        [string] $GithubRepository
+    )
+
+    if ($Name -eq $null) { throw "Name can't be empty" }
+    if (Test-Path $Name) { throw "Package with that name already exists" }
+    if (!(Test-Path _template)) { throw "Template for the packages not found" }
+    $LowerName = $Name.ToLower() -replace ' ','-'
+    cp _template $LowerName -Recurse
+
+    Move-Item "$LowerName/template.nuspec" "$LowerName/$LowerName.nuspec" -Force;
+    ../setup/Update-IconUrl.ps1 -Name "$LowerName" -GithubRepository $GithubRepository;
+    $nuspec = gc -Encoding utf8 "$LowerName/$LowerName.nuspec";
+
+    Write-Verbose 'Fixing nuspec'
+    $nuspec = $nuspec -replace '<id>.+',               "<id>$LowerName</id>"
+    $nuspec = $nuspec -replace '<title>.+',            "<title>$Name</title>"
+    $nuspec = $nuspec -replace '<packageSourceUrl>.+', "<packageSourceUrl>https://github.com/$GithubRepository/tree/master/automatic/$LowerName</packageSourceUrl>"
+    $nuspec | Out-File -Encoding UTF8 "$LowerName\$LowerName.nuspec"
+
+    switch ($Type)
+    {
+        'Installer' {
+            Write-Verbose 'Using installer template'
+            rm "$LowerName\tools\chocolateyInstallZip.ps1"
+            mv "$LowerName\tools\chocolateyInstallExe.ps1" "$LowerName\tools\chocolateyInstall.ps1"
+        }
+        'Portable' {
+            Write-Verbose 'Using portable template'
+            rm "$LowerName\tools\chocolateyInstallExe.ps1"
+            mv "$LowerName\tools\chocolateyInstallZip.ps1" "$LowerName\tools\chocolateyInstall.ps1"
+        }
+    }
+
+    Write-Verbose 'Fixing chocolateyInstall.ps1'
+    $installer = gc "$LowerName\tools\chocolateyInstall.ps1"
+    $installer = $installer -replace "(?i)(^\s*softwareName\s*=\s*)('.*')", "`$1'$($Name)'"
+    $installer -replace "(?i)(^\s*packageName\s*=\s*)('.*')", "`$1'$($LowerName)'" | sc "$LowerName\tools\chocolateyInstall.ps1"
+}
+
+New-Package $Name $Type -GithubRepository AdmiringWorm/chocolatey-packages -Verbose
---
+++ scripts/au_extensions.psm1
@@ -0,0 +1,20 @@
+# Export all the extensions that are meant to be used
+# within a au update script here.
+
+# We just specify the functions we want to export
+# but the file containing the functions is expected
+# to be named using the same name.
+$funcs = @(
+  'Set-DescriptionFromReadme'
+  'Update-Changelog'
+  'Update-ChangelogVersion'
+  'Update-Metadata'
+  'Get-LatestGithubReleases'
+  'Get-AllGithubReleases'
+  "Update-OnETagChanged"
+)
+
+$funcs | % {
+  . "$PSScriptRoot\$_.ps1"
+  Export-ModuleMember -Function $_
+}
