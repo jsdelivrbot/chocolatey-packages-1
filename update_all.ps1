91971bff9f6ff0b51c54e3de17b058a8a0561aa4
Nicholas Cecere <nick@thebitop.net>
2018-09-06 18:33:41 -0400

    more testing

--- update_all.ps1
+++ update_all.ps1
@@ -8,7 +8,7 @@ $Options = [ordered]@{
     Timeout       = 100                                     #Connection timeout in seconds
     UpdateTimeout = 1200                                    #Update timeout in seconds
     Threads       = 10                                      #Number of background jobs to use
-    Push          = $Env:au_Push -eq 'true'                 #Push to chocolatey
+    Push          = $Env:au_Push                            #Push to chocolatey
     PluginPath    = ''                                      #Path to user plugins
 
     Report = @{
