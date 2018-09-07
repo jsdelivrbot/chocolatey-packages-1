638a91e5ec97bf3f0f038074f977063927ca1960
Nicholas Cecere <nick@thebitop.net>
2018-09-06 23:04:39 -0400

    addind r

---
+++ automatic/r.project/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = 'r.project'
+  fileType      = 'exe'
+  softwareName  = 'R for Windows*'
+  file          = "$toolsDir\R-3.5.1-win.exe"
+  silentArgs    = "/SILENT /SUPPRESSMSGBOXES /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
+  validExitCodes= @(0)
+}
+
+$pp = Get-PackageParameters
+if ($pp.UseInf) {
+  if (Test-Path "$($pp.UseInf)") {
+    Write-Host "Using existing configuration file at '$($pp.UseInf)'"
+    $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /LOADINF=`"$($pp.UseInf)`""
+  } else {
+    Write-Host "Creating new configuration file at '$($pp.UseInf)'"
+    $packageArgs['silentArgs'] = "$($packageArgs['silentArgs']) /SAVEINF=`"$($pp.UseInf)`""
+  }
+}
+
+Install-ChocolateyInstallPackage @packageArgs
---
+++ automatic/r.project/update.ps1
@@ -0,0 +1,33 @@
+﻿. "$PSScriptRoot/../r/update.ps1"
+import-module "$PSScriptRoot\..\..\scripts\au_extensions.psm1"
+
+$softwareName = 'R for Windows*'
+
+function global:au_BeforeUpdate {
+  $Latest.ChecksumType32 = 'sha256'
+  Get-RemoteFiles -FileNameBase $Latest.FileName32.TrimEnd('.exe') -NoSuffix -Purge
+}
+
+function global:au_AfterUpdate { Update-Changelog -useIssueTitle }
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt"      = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL32)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum32)"
+    }
+
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(`"`[$]toolsDir\\).*`""        = "`${1}$($Latest.FileName32)`""
+      "(?i)(^\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
+    }
+    ".\r.project.nuspec"            = @{
+      "(\<id\>).*(\<\/id\>)" = "`${1}R.Project`${2}"
+    }
+  }
+}
+
+update -ChecksumFor none
